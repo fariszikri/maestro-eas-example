09b2ff1c80db7b0888d92a7a2f7f5af0d720f717
_fahmi <afahmiparidin@gmail.com>
2024-05-07 18:39:30 +0800

    Update eas-build-on-success.sh
    added comment

--- eas-build-on-success.sh
+++ eas-build-on-success.sh
@@ -12,8 +12,10 @@ then
    sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
    export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
 
+   # path for ipa
    APP_EXECUTABLE_PATH=/Users/expo/workingdir/build/ios/build/Build/Products/Release-iphonesimulator/MaestroEasExample.app
 else
+   # path for apk
    APP_EXECUTABLE_PATH=/home/expo/workingdir/build/android/app/build/outputs/apk/release/app-release.apk
 fi
 
