1fe960c63ad758b6b077bece421da0759e607ae2
_fahmi <afahmiparidin@gmail.com>
2024-05-07 18:36:02 +0800

    Update eas-build-on-success.sh
    added comment on maestro apk path

--- eas-build-on-success.sh
+++ eas-build-on-success.sh
@@ -11,7 +11,7 @@ then
    echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
    sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
    export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
-
+   # path in Maestro
    APP_EXECUTABLE_PATH=/Users/expo/workingdir/build/ios/build/Build/Products/Release-iphonesimulator/MaestroEasExample.app
 else
    APP_EXECUTABLE_PATH=/home/expo/workingdir/build/android/app/build/outputs/apk/release/app-release.apk
